b891684175cc233edb4aee392cef9ca4ecd3fed6
Andree Torres <84477362+PatloAndree@users.noreply.github.com>
2023-07-24 13:46:51 -0500

    removed

--- Shared/Axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    //COLOCAR TU DIREECIÓN IP : PUERTO / CARPETA 
-    // baseURL: "https://teleinterconsulta.energenioperu.com/",
-    baseURL: "http://192.168.18.30:82/asistencias/",
-    timeout:  1000,
-    headers: { 'Content-Type': 'application/json' }
-});
